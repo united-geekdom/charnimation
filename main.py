e11b3358ec3ccba3c377f4a3f69e8745387f0086
fisik-yum <govind.sriram@aol.com>
2022-06-11 08:25:15 -0700

    test

--- main.py
+++
@@ -1,8 +0,0 @@
-from reader import chmnread
-import sys
-if len(sys.argv) == 1:
-    print("Please specify a filepath")
-elif len(sys.argv) > 2:
-    print("Cannot understand argument")
-else:
-    chmnread(sys.argv[1])
