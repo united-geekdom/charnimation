03049ceebe5c96f2488729233c162cb5dbf01259
united-geekdom <kirannithilan@gmail.com>
2022-06-26 19:48:02 +0530

    Add animation capability

--- frameparser.py
+++ frameparser.py
@@ -1,16 +1,25 @@
 import linecache
 import json
 import sys
-def parseFrame(l, h):
+import time
+def parseFrame(l, h, au):
     toPrint = ""
     for n in range((l-1)*(h+1) + 1, l*(h+1) + 1):
         if n != (l-1)*(h+1) + 1:
             toPrint += linecache.getline("filedata/frames.txt", n)
+    toPrint += f"\nBy: {au}\n"
     return toPrint
 def returnFrame():
     metadata = json.load(open("filedata/metadata.json"))
     height = metadata["dimensions"][1]
     fl = metadata["frames"]
+    intrv = 1/metadata["fps"]
+    if len(metadata["author"]) == 1:
+        author = metadata["author"][0]
+    else:
+        author = ", ".join(map(str, metadata["author"]))
+
     for c in range(fl):
-        sys.stdout.write("\r" + parseFrame(c+1, height))
-        sys.stdout.flush()
+        print('\033[H\033[J', end='')
+        print(parseFrame(c+1, height, author), end="\r")
+        time.sleep(intrv)
