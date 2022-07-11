74599043caec997e36efe4ed0c618a8df85998cd
united-geekdom <kirannithilan@gmail.com>
2022-07-11 15:10:24 +0530

    transfers

--- frameparser.py
+++ frameparser.py
@@ -2,18 +2,19 @@ import linecache
 import json
 import sys
 import time
-def parseFrame(l, h, au):
+def parseFrame(l, h, ti, au):
     toPrint = ""
     for n in range((l-1)*(h+1) + 1, l*(h+1) + 1):
         if n != (l-1)*(h+1) + 1:
             toPrint += linecache.getline("filedata/frames.txt", n)
-    toPrint += f"\nBy: {au}\n"
+    toPrint += f"\nTitle: {ti}\nBy: {au}\n"
     return toPrint
 def returnFrame():
     metadata = json.load(open("filedata/metadata.json"))
     height = metadata["dimensions"][1]
     fl = metadata["frames"]
     intrv = 1/metadata["fps"]
+    title = metadata["title"]
     if len(metadata["author"]) == 1:
         author = metadata["author"][0]
     else:
@@ -21,5 +22,5 @@ def returnFrame():
 
     for c in range(fl):
         print('\033[H\033[J', end='')
-        print(parseFrame(c+1, height, author), end="\r")
+        print(parseFrame(c+1, height, title, author), end="\r")
         time.sleep(intrv)
